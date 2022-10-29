87e8806aeafbe1c2fdcb6c35b23b94110eed2330
Brian Nduhiu <briannduhiu@gmail.com>
2022-10-29 21:50:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main  {
+    public static void main(String[] args){
+        char letter = 'A';
+        byte constant = 4;
+        short percent = 83;
+        int amount = 34993322;
+        float interest = 12.5f;
+        double pi = 3.14159265359d;
+        boolean hasAccount = true;
+
+        System.out.println("Char - "+letter);
+        System.out.println("byte - "+constant);
+        System.out.println("Short - "+percent);
+        System.out.println("Int - "+amount);
+        System.out.println("Float - "+interest);
+        System.out.println("Double - "+pi);
+        System.out.println("Boolean - "+hasAccount);
+
+
+    }
+}
