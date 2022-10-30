c68a2cc824bba28cae0dff0070570bea641c53d4
Brian Nduhiu <briannduhiu@gmail.com>
2022-10-31 02:29:09 +0300

    Multi Dimensional Arrays

--- src/arrays/Arrays.java
+++ src/arrays/Arrays.java
@@ -10,5 +10,15 @@ public class Arrays {
         list[3]= 31;
 
         System.out.println(java.util.Arrays.toString(list));
+
+        int[][] table = {{1,5,4},{6,7,2},{9,3,2}};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(table[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+        
     }
 }
